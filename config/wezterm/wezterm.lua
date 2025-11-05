68daec90b8ef50aeb70313b67a917d0d191163fd
kanChome <hellbird1998@gmail.com>
2025-11-06 02:44:58 +0900

    fix: ファイル名の修正

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -5,10 +5,16 @@ local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
 -- This is where you actually apply your config choices.
+config.font = wezterm.font('HackGenNerd')
 
--- For example, changing the initial geometry for new windows:
-config.initial_cols = 120
-config.initial_rows = 28
+-- 背景の非透過率（1なら完全に透過させない）
+config.window_background_opacity = 0.80
+
+-- 背景のぼかし
+config.macos_window_background_blur = 20
+
+-- タイトルバーの非表示
+config.hide_tab_bar_if_only_one_tab = true
 
 -- or, changing the font size and color scheme.
 config.font_size = 10
